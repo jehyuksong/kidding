af37a5806e8404260137f94f83ccc3c5a38d1fc0
SONG <SONG@SONGui-MacBook-Pro.local>
2020-08-18 00:48:51 +0900

    master
    master

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const jehyukImage = document.querySelector(".jehyuk-image");
 const gosuImage = document.querySelector(".gosu-image");
 const scream = new Audio("sounds/scream.mp3");
+const btn = document.createElement("button");
+const btnText = document.createTextNode("닫기");
 
 jehyukImage.addEventListener("click", function () {
   if (jehyukImage.getAttribute("src") === "images/jehyuk.png") {
@@ -17,8 +19,21 @@ gosuImage.addEventListener("click", function () {
     gosuImage.setAttribute("src", "images/gosuPick.png");
     scream.play();
     gosuImage.classList.add("bigger-gosu");
+    btn.appendChild(btnText);
+    document.body.appendChild(btn);
+    btn.classList.add("close-button");
+    btn.classList.remove("invisible");
+    btn.addEventListener("click", function () {
+      for (let i = 0; i < 100; i++) {
+        alert("선택을 잘해야지ㅎㅎ");
+      }
+    });
   } else {
     gosuImage.setAttribute("src", "images/gosu.png");
     gosuImage.classList.remove("bigger-gosu");
+    for (let i = 0; i < 100; i++) {
+      alert("선택을 잘해야지ㅎㅎ");
+    }
+    btn.classList.add("invisible");
   }
 });
